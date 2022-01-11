2452f5dea12a1ac75383683178aa22fb1595611f
babboe1 <ayodelaustinab@gmail.com>
2022-01-11 04:33:28 +0100

    refactor: create DOM variables

---
+++ assets/script/app.js
@@ -0,0 +1,7 @@
+const BODY = document.getElementById('body_container');
+const BACKDROP = document.getElementById('backdrop');
+const GRATITUDE = document.getElementById('gratitude').parentElement;
+const ACTION_BTN = document.getElementById('action_btn');
+const OK_BTN = document.getElementById('ok_btn');
+const USER_INPUT = document.getElementById('email_id');
+const ERROR_TEXT = document.querySelector('.ErrorText');
